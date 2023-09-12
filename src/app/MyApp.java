4d9ea82df8ebfa64605ce3ca24422912c0954024
Nikolai <kolya.kovalets@ukr.net>
2023-09-12 22:54:28 +0200

    Координаты Лас - Вегаса

---
+++ src/app/MyApp.java
@@ -0,0 +1,15 @@
+package app;
+
+public class MyApp {
+    static double latitude;
+    static double longitude;
+
+    public static void main(String[] args) {
+        latitude = 36.0805833;
+        longitude = -115.1374072;
+        System.out.println("Широта " + latitude);
+        System.out.println("Довгота " + longitude);
+
+
+    }
+}
