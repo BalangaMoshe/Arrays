4c2a637858b820964ff4e192b9eb97a539760a63
Mykola Kovalets <kolya.kovalets@ukr.net>
2023-09-14 14:02:22 +0200

    git test

---
+++ src/app/kajds.java
@@ -0,0 +1,7 @@
+package app;
+
+public class kajds {
+    public static void main(String[] args) {
+        System.out.println("jsdfjvejhbekfj");
+    }
+}
---
+++ src/app/sdefrae.java
@@ -0,0 +1,7 @@
+package app;
+
+public class sdefrae {
+    public static void main(String[] args) {
+        System.out.println("skjdghjchsg");
+    }
+}
