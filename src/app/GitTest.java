9243df9466313c0bc3bb6cfe248758597a6a63c5
Mykola Kovalets <kolya.kovalets@ukr.net>
2023-09-14 13:50:18 +0200

    git test

--- src/app/GitTest.java
+++ src/app/GitTest.java
@@ -3,9 +3,11 @@ package app;
 public class GitTest {
     static String messege;
     static int blaBla;
+    static String blablabla;
     public static void main(String[] args) {
         messege = "Hallo Leuter ";
         blaBla = 10235;
-    System.out.println(messege + blaBla);
+        blablabla = " I mag GIT";
+    System.out.println(messege + blaBla + blablabla);
     }
 }
